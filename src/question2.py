081ff1cf74ec90ff6a45fa9e8612b020efb23c66
Francielle Fernandes Rodrigues <francielle.rodrigues@taglivros.com.br>
2024-03-30 13:21:12 -0300

    fix:Corrigir algoritmo de ordenação Merge Sort

--- src/question2.py
+++ src/question2.py
@@ -14,7 +14,7 @@ def merge_sort(arr):
         i = j = k = 0
 
         while i < len(left_half) and j < len(right_half):
-            if left_half[i] > right_half[j]:
+            if left_half[i] < right_half[j]:
                 arr[k] = left_half[i]
                 i += 1
             else:
