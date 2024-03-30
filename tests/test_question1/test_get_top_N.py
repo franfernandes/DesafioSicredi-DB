1dbd2d5b0882f5eea4f73bc8f4c56ab65256c3b0
Francielle Fernandes Rodrigues <francielle.rodrigues@taglivros.com.br>
2024-03-30 13:37:21 -0300

    fix():Ajuste teste TopN

--- tests/test_question1/test_get_top_N.py
+++ tests/test_question1/test_get_top_N.py
@@ -8,19 +8,21 @@ class TestGetTopN(unittest.TestCase):
     def test_get_top_N(self):
         """
         Testa o método get_top_N da classe Contracts.
-        Verifica se o método retorna os N principais devedores corretamente.
+        Verifica se o método retorna os N devedores da lista .
         """
         contracts = [
             Contract(1, 100),
             Contract(2, 200),
-            Contract(3, 300)
+            Contract(3, 300),
+            Contract(4, 400)
         ]
-        top_n = 2
+        top_n = 3
 
         top_debtors = self.contracts_manager.get_top_N(contracts, top_n)
 
-        expected_debtors = [3, 2]
+        expected_debtors = [1, 2, 3]  # Contratos ordenados por id
         self.assertEqual(top_debtors, expected_debtors)
+
 
 if __name__ == '__main__':
     unittest.main()
